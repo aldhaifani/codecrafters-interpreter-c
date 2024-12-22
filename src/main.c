c522370715426c29ca7a362996b73aba5f531b87
Tareq <tareqaldhaifani@gmail.com>
2024-12-22 11:54:55 +0400

    Scanning for curly braces

--- src/main.c
+++ src/main.c
@@ -2,40 +2,53 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *read_file_contents(const char *filename);
+char* read_file_contents(const char* filename);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[])
+{
     // Disable output buffering
     setbuf(stdout, NULL);
     setbuf(stderr, NULL);
 
-    if (argc < 3) {
+    if (argc < 3)
+    {
         fprintf(stderr, "Usage: ./your_program tokenize <filename>\n");
         return 1;
     }
 
-    const char *command = argv[1];
+    const char* command = argv[1];
 
-    if (strcmp(command, "tokenize") == 0) {
+    if (strcmp(command, "tokenize") == 0)
+    {
+        char* file_contents = read_file_contents(argv[2]);
 
-        char *file_contents = read_file_contents(argv[2]);
-
-        if (strlen(file_contents) > 0) {
-            for (int i = 0; i < strlen(file_contents); i++) {
-                switch (file_contents[i]) {
-                    case '(':
-                        printf("LEFT_PAREN ( null\n");
-                        break;
-                    case ')':
-                        printf("RIGHT_PAREN ) null\n");
-                        break;
+        if (strlen(file_contents) > 0)
+        {
+            for (int i = 0; i < strlen(file_contents); i++)
+            {
+                switch (file_contents[i])
+                {
+                case '(':
+                    printf("LEFT_PAREN ( null\n");
+                    break;
+                case ')':
+                    printf("RIGHT_PAREN ) null\n");
+                    break;
+                case '{':
+                    printf("LEFT_BRACE { null\n");
+                    break;
+                case '}':
+                    printf("RIGHT_BRACE } null\n");
+                    break;
                 }
             }
         }
         printf("EOF  null\n"); // Placeholder, remove this line when implementing the scanner
-        
+
         free(file_contents);
-    } else {
+    }
+    else
+    {
         fprintf(stderr, "Unknown command: %s\n", command);
         return 1;
     }
@@ -43,9 +56,11 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-char *read_file_contents(const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
+char* read_file_contents(const char* filename)
+{
+    FILE* file = fopen(filename, "r");
+    if (file == NULL)
+    {
         fprintf(stderr, "Error reading file: %s\n", filename);
         return NULL;
     }
@@ -54,15 +69,17 @@ char *read_file_contents(const char *filename) {
     long file_size = ftell(file);
     rewind(file);
 
-    char *file_contents = malloc(file_size + 1);
-    if (file_contents == NULL) {
+    char* file_contents = malloc(file_size + 1);
+    if (file_contents == NULL)
+    {
         fprintf(stderr, "Memory allocation failed\n");
         fclose(file);
         return NULL;
     }
 
     size_t bytes_read = fread(file_contents, 1, file_size, file);
-    if (bytes_read < file_size) {
+    if (bytes_read < file_size)
+    {
         fprintf(stderr, "Error reading file contents\n");
         free(file_contents);
         fclose(file);
